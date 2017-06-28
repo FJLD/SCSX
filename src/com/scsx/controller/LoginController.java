65548cb773209233d27ed02b7db913c47308d27a
daishoui <daishoui@outlook.com>
2017-06-28 23:38:04 +0800

    完善了登录验证

--- src/com/scsx/controller/LoginController.java
+++ src/com/scsx/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.scsx.controller;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.scsx.domain.User;
@@ -16,59 +19,56 @@ import com.scsx.util.DesUtil;
 
 @Controller
 public class LoginController {
-	@RequestMapping("DoLogin")
-	public ModelAndView DoLogin(ModelAndView modelAndView, HttpServletRequest request,HttpServletResponse response, User user) throws Exception{
-		String code = request.getParameter("code");
-		if(user == null || code == null){
-			modelAndView.addObject("error", "请输入登录信息");
-			modelAndView.setViewName("test");
-			return modelAndView;
-		}
-		//从session中获取验证码
-		String scode = (String) request.getSession().getAttribute("scode");
-		System.out.println(user.getUNAME()+" "+user.getPW()+" "+scode+" "+code);
-		if(!scode.equalsIgnoreCase(code)){
-			modelAndView.addObject("message", "验证码错误。");
-			modelAndView.setViewName("login");
-			return modelAndView;
-		}
-		user.setPW(DesUtil.getDesUtilInstance().encrypt(user.getPW()));	//将登录的用户密码加密
-		System.out.println(user.getUNAME()+" "+user.getPW()+" "+scode+" "+code);
-		Cookie ck1 = new Cookie("username",user.getUNAME());
-		Cookie ck2 = new Cookie("password",user.getPW());
-		ck1.setPath("/");
-		ck2.setPath("/");
-		System.out.println("username="+user.getUNAME()+" password="+user.getPW());
-		//处理业务逻辑
+	@RequestMapping("DoLogin.do")
+	public ModelAndView DoLogin(ModelAndView modelAndView, HttpServletRequest request,HttpServletResponse response) throws Exception{
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
 		if(UserService.getUserServiceInstance().confirm(user)){
 			if(user.getPOWER().equals("用户")){
-				user = UserService.getUserServiceInstance().getUserFromUNAME(user.getUNAME());
-				ck1.setMaxAge(Integer.MAX_VALUE);//设置Cookie的有效保存时间
-				ck2.setMaxAge(Integer.MAX_VALUE);//设置Cookie的有效保存时间
-				HttpSession session = request.getSession();
-				session.setAttribute("user", user);
-				response.addCookie(ck1);
-				response.addCookie(ck2);
-				System.out.println(user.getHEADIMAGE());
 				modelAndView.addObject("headImage", user.getHEADIMAGE());
 				modelAndView.setViewName("WEB-INF/ordinary_user/index");
 				return modelAndView;
 			} else {
-				user = UserService.getUserServiceInstance().getUserFromUNAME(user.getUNAME());
-				ck1.setMaxAge(Integer.MAX_VALUE);//设置Cookie的有效保存时间
-				ck2.setMaxAge(Integer.MAX_VALUE);//设置Cookie的有效保存时间
-				HttpSession session = request.getSession();
-				session.setAttribute("user", user);
-				response.addCookie(ck1);
-				response.addCookie(ck2);
 				modelAndView.addObject("headImage", user.getHEADIMAGE());
 				modelAndView.setViewName("WEB-INF/admin/index");
 				return modelAndView;
 			}
 		}
-		modelAndView.addObject("headImage", user.getHEADIMAGE());
-		modelAndView.addObject("message", "用户名或密码错误。");
-		modelAndView.setViewName("login");
+		modelAndView.addObject("error", "用户名或密码错误。");
+		modelAndView.setViewName("test");
 		return modelAndView;
+	}
+	
+	@RequestMapping(value = "Login.do",method=RequestMethod.POST)
+	public void Login(HttpServletRequest request,HttpServletResponse response, User user, PrintWriter out) throws Exception{
+		String code = request.getParameter("code");
+		if(code==null){
+			out.print("{\"status\":1000}");
+			return;
+		}
+		if(user == null || user.getUNAME() == null || user.getPW()==null){
+			out.print("{\"status\":1001}");
+			return;
+		}
+		//从session中获取验证码
+		String scode = (String) request.getSession().getAttribute("scode");
+		if(!scode.equalsIgnoreCase(code)){
+			out.print("{\"status\":1002}");
+			return;
+		}
+		user.setPW(DesUtil.getDesUtilInstance().encrypt(user.getPW()));	//将登录的用户密码加密
+		if(UserService.getUserServiceInstance().confirm(user)){
+			if(user.getPOWER().equals("用户") || user.getPOWER().equals("管理员")){
+				user = UserService.getUserServiceInstance().getUserFromUNAME(user.getUNAME());
+				HttpSession session = request.getSession();
+				session.setAttribute("user", user);
+				out.print("{\"status\":200}");
+				return;
+			}
+		}
+		else{
+			out.print("{\"status\":1003}");
+			return;
+		}
 	}
 }
