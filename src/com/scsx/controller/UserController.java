42be57fb63c8ded3099f68ff09f13d1910fa7c16
daishoui <daishoui@outlook.com>
2017-06-29 08:10:31 +0800

    删除了springmvc

--- src/com/scsx/controller/UserController.java
+++ src/com/scsx/controller/UserController.java
@@ -7,11 +7,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
 import com.scsx.domain.User;
@@ -128,5 +131,92 @@ public class UserController {
 				+ "\nnew password: " + PW);
 		boolean success = UserService.getUserServiceInstance().updateUserPW(UNO, PW);
 		out.write(success? "true" : "false");
+	}
+	
+	@RequestMapping("/Profile.do")
+	public ModelAndView showPersonalInfo(HttpServletRequest request){
+		ModelAndView modelAndView = new ModelAndView();
+		String username ="";
+		String password = "";
+		//得到客户端保存的Cookie数据
+		Cookie[] cookies = request.getCookies();
+		for (int i = 0;cookies!=null && i < cookies.length; i++) {
+			if("username".equals(cookies[i].getName())){
+				username = cookies[i].getValue();
+			}
+			if("password".equals(cookies[i].getName())){
+				password = cookies[i].getValue();
+			}
+		}
+		User user = UserService.getUserServiceInstance().getUserFromUNAMEAndPW(username, password);
+		if(user != null){
+			request.setAttribute("user", user);
+			modelAndView.addObject("user", user);
+			modelAndView.setViewName("/WEB-INF/ordinary_user/profile");
+			return modelAndView;
+		}
+		modelAndView.addObject("error", "后台验证错误");
+		modelAndView.setViewName("test");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/Register.do")
+	public String register(Model model, User user) throws Exception{
+		if(user==null || user.getUNAME()==null || user.getID() == null || user.getNAME() == null 
+				|| user.getPW() == null || user.getUPHONE()==null){	
+			System.out.println(user.getUNAME()+" "+ user.getID()+" "+ user.getNAME() +" "+ user.getPW() +" "+ user.getUPHONE());
+			model.addAttribute("error", "填入信息信息不完整");
+			return "test";
+		}
+		user.setPOWER("用户");
+		user.setHEADIMAGE("images/avatar.jpg");
+		user.setPW(DesUtil.getDesUtilInstance().encrypt(user.getPW()));	//将用户密码加密后保存
+		if(UserService.getUserServiceInstance().isValidRegisterUNAME(user.getUNAME()) == false){
+			model.addAttribute("error", "用户名已存在");
+			return "test";
+		}
+		UserService.getUserServiceInstance().insertUser(user);
+		model.addAttribute("user", user);
+		return "login";
+	}
+	
+	@RequestMapping("/ListUsers.do")
+	public String userList(HttpServletRequest request) throws IOException {// 管理员用于显示所有的用户
+		User user = (User) request.getSession().getAttribute("user");
+		if (UserService.getUserServiceInstance().confirm(user)) {
+			return "/WEB-INF/admin/users_list";
+		}
+		return "test";
+	}
+	
+	@RequestMapping("/AllRecords.do")
+	public String AllRecords(HttpServletRequest request) {
+		User user = (User) request.getSession().getAttribute("user");
+		if (UserService.getUserServiceInstance().confirm(user)) {
+			return "WEB-INF/admin/records";
+		}
+		return "test";
+	}
+	
+	@RequestMapping("/QuestionsManager.do")
+	public String QuestionsManager(HttpServletRequest request) {
+		User user = (User) request.getSession().getAttribute("user");
+		if (UserService.getUserServiceInstance().confirm(user)) {
+			return "WEB-INF/admin/questions";
+		}
+		return "test";
+	}
+
+	@RequestMapping("/hello.do")
+	public String forword(Model model) {
+		// model.addAttribute("message", "同学们好！");
+		return "login";
+	}
+
+	@RequestMapping("/test.do")
+	public void backword(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		request.setAttribute("hello12", "world");
+		// return "test";
+		response.getWriter().write("<h1>world</h1>");
 	}
 }  
